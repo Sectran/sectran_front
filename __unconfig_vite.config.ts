56cd114c67df638f279a2d7d08c135734213cac1
律清政 <1248869969@qq.com>
2024-12-13 19:13:38 +0800

    提交

---
+++ __unconfig_vite.config.ts
@@ -0,0 +1,72 @@
+
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) { __unconfig_data = data };
+__unconfig_stub.default = (data = {}) => { __unconfig_data = data };
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from "@vitejs/plugin-vue-jsx";
+// import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import { join } from "path";
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
+const __unconfig_default =  defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		base: './',
+		plugins: [
+			vue(),
+			vueJsx(),
+			// VueSetupExtend(),
+			Components({
+				resolvers: [
+					AntDesignVueResolver({
+						importStyle: false, // css in js
+					}),
+				],
+			}),
+		],
+		
+		server: {
+			proxy: {
+				[env.VITE_PROXY_BASE_URL]: {
+					target: env.VITE_API_URL,
+					changeOrigin: true,
+					rewrite: (path) => path.replace(new RegExp(`^${env.VITE_PROXY_BASE_URL}`), ''),
+				},
+				[env.VITE_PROXY_BASE_File]: {
+					target: env.VITE_Chard_FILE,  
+					changeOrigin: true,
+					rewrite: (path) => path.replace(new RegExp(`^${env.VITE_PROXY_BASE_File}`), ''),
+				},
+			},
+		},
+
+		resolve: {
+			alias: {
+				'@': join(__dirname, "src"),
+				'views': join(__dirname, "src/views"),
+			}
+		},
+		css: {
+			// css预处理器
+			preprocessorOptions: {
+				less: {
+					charset: false,
+					// additionalData: '@import "./src/assets/style/global.less";',
+				},
+			},
+		},
+
+		optimizeDeps: {
+			include: ['schart.js']
+		},
+		define: { __VUE_I18N_FULL_INSTALL__: true, __VUE_I18N_LEGACY_API__: true, __INTLIFY_PROD_DEVTOOLS__: false },
+	};
+});
+
+
+
+
+
+if (typeof __unconfig_default === "function") __unconfig_default(...[{"command":"serve","mode":"development"}]);export default __unconfig_data;
